7afbc2cddf12d3312a93020bda1e7de6c3d3088d
magdalenapetrushevska <magdalenapetrusevska9@gmail.com>
2022-04-10 20:31:13 +0200

    Change path

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://book-rental-app.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*',
         'Authorization': localStorage.getItem("JWT")
